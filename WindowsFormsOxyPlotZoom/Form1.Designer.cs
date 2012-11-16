63cc0471bc42f5af4c393d12e345efb4f3ed6aa7
JohnnyPP <johnnnyppp@gmail.com>
2012-11-17 00:28:05 +0100

    Initial release. http://oxyplot.codeplex.com/discussions/402272

---
+++ WindowsFormsOxyPlotZoom/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace WindowsFormsOxyPlotZoom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plot1 = new OxyPlot.WindowsForms.Plot();
+            this.SuspendLayout();
+            // 
+            // plot1
+            // 
+            this.plot1.KeyboardPanHorizontalStep = 0.1D;
+            this.plot1.KeyboardPanVerticalStep = 0.1D;
+            this.plot1.Location = new System.Drawing.Point(9, 9);
+            this.plot1.Margin = new System.Windows.Forms.Padding(0);
+            this.plot1.Name = "plot1";
+            this.plot1.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plot1.Size = new System.Drawing.Size(1006, 491);
+            this.plot1.TabIndex = 1;
+            this.plot1.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plot1.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plot1.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1024, 509);
+            this.Controls.Add(this.plot1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private OxyPlot.WindowsForms.Plot plot1;
+    }
+}
+
---
+++ WindowsFormsOxyPlotZoom/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OxyPlot;
+
+namespace WindowsFormsOxyPlotZoom
+{
+    public partial class Form1 : Form
+    {
+        class MyValue : IComparable {
+            public DateTime Date { get; set; }
+            public double Value { get; set; }
+            public MyValue(DateTime d) {
+                Date = new DateTime(d.Year, d.Month, d.Day);
+            }
+            public MyValue(DateTime d, double v) {
+                Date = new DateTime(d.Year, d.Month, d.Day);
+                Value = v;
+            }
+            int IComparable.CompareTo(object to) {
+                return Date.CompareTo(((MyValue)to).Date);
+            }
+        }
+            
+        DateTimeAxis xAxisDateTime;
+        LinearAxis yAxisValues;
+        List<MyValue> myValues;
+        PlotModel plotModel;
+        LineSeries lineSeries;
+
+        public Form1() {
+            InitializeComponent();
+
+            myValues = new List<MyValue>();
+            DateTime startDate = new DateTime(2000, 1, 1);
+            double startValue = 100;
+            Random r = new Random();
+            for (int i = 0; i < 1000; i++, startDate = startDate.AddDays(1), startValue += (r.NextDouble() - 0.5)) {
+                myValues.Add(new MyValue(startDate, startValue));
+            }
+
+            plotModel = new PlotModel("Random chart");
+
+            xAxisDateTime = new DateTimeAxis() { 
+                IntervalType = DateTimeIntervalType.Auto, 
+                MinorIntervalType = DateTimeIntervalType.Days, 
+                MajorGridlineStyle = LineStyle.Solid, 
+                MinorGridlineStyle = LineStyle.Dot, 
+                CalendarWeekRule = System.Globalization.CalendarWeekRule.FirstFourDayWeek, 
+                FirstDayOfWeek = DayOfWeek.Monday, 
+                Position = AxisPosition.Bottom 
+            };
+            //Set the min/max values of the x-axis to my min/max date to prevent zoom/pan beyond data series
+            xAxisDateTime.AbsoluteMinimum = myValues[0].Date.ToOADate();
+            xAxisDateTime.AbsoluteMaximum = myValues[myValues.Count - 1].Date.ToOADate();
+            
+            //calling user defined event on change
+            xAxisDateTime.AxisChanged += xAxisDateTime_AxisChanged;
+
+            yAxisValues = new LinearAxis() { 
+                MajorGridlineStyle = LineStyle.Solid, 
+                MinorGridlineStyle = LineStyle.Dot 
+            };
+
+            plotModel.Axes.Add(xAxisDateTime);
+            plotModel.Axes.Add(yAxisValues);
+
+            lineSeries = new LineSeries {
+                Title = "Values",
+                Color = OxyColor.FromArgb(255, 78, 154, 6),
+                MarkerType = MarkerType.None,
+                StrokeThickness = 1,
+                DataFieldX = "Date",
+                DataFieldY = "Value",
+                ItemsSource = myValues
+            };
+
+            plotModel.Series.Add(lineSeries);
+            plot1.Model = plotModel;
+
+        }
+
+        void xAxisDateTime_AxisChanged(object sender, AxisChangedEventArgs e) {
+            DateTimeAxis axis = sender as DateTimeAxis;
+
+            //save the current min/max date values
+            DateTime dtMax = DateTime.FromOADate(axis.ActualMaximum);
+            DateTime dtMin = DateTime.FromOADate(axis.ActualMinimum);
+
+            double minValue = double.MaxValue;
+            double maxValue = double.MinValue;
+
+            //BinarySearch, because the xAxisDateTime is sorted
+            int idxMin = myValues.BinarySearch(new MyValue(dtMin));
+            int idxMax = myValues.BinarySearch(new MyValue(dtMax));
+
+            //if we can not find an exact location (result is negativ) we can use the complement 
+            //see BinarySearch help for further explanation
+            if (idxMin < 0) idxMin = ~idxMin;
+            if (idxMax < 0) idxMax = ~idxMax;
+
+            //find the corresponding min/max values in the selected intervall
+            for (int i = idxMin; i < idxMax; i++) {
+                minValue = Math.Min(minValue, myValues[i].Value);
+                maxValue = Math.Max(maxValue, myValues[i].Value);
+            }
+
+            //set y-axis min/max and redraw the chart
+            yAxisValues.Zoom(minValue, maxValue);
+            plotModel.RefreshPlot(true);
+        }
+    }
+}
+   
